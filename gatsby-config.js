8e9c3dc61f73d5d72430b4ac10643802b2110dc8
Dan Boyle <danboyle8637@gmail.com>
2019-07-01 09:16:59 -0400

    Updated gatsby-config to take out summer filesystem locations.

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,20 +125,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `SummerSlimDownImages`,
-        path: `${__dirname}/src/images/SummerSlimDown`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `SummerSlimDownCopy`,
-        path: `${__dirname}/src/content/SummerSlimDown`,
-      },
-    },
-    {
       resolve: 'gatsby-source-graphql',
       options: {
         typeName: `GCMS`,
